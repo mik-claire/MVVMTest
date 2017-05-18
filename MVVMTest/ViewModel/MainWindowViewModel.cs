9bbc9a0bda8a1467e06b70d9595cce9227d25bdf
mik-claire <mekakushi.mkz@gmail.com>
2017-05-18 21:14:17 +0900

    Initial commit.

---
+++ MVVMTest/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMTest.ViewModel
+{
+	public class MainWindowViewModel
+	{
+		public string Text { get; set; }
+
+		public MainWindowViewModel()
+		{
+			this.Text = "Hello, world!";
+		}
+	}
+}
